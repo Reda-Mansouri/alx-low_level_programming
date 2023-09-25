e7ae581460f477c3a623a1a3a057c21dff87495e
Reda-Mansouri <redamnasouri332@gmail.com>
2023-09-25 19:46:09 +0100

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+       char c,b;
+       for ( c = 'a' ; c <= 'z' ; c++)
+       {
+	       putchar (c);
+       }
+        for ( b = 'A' ; b <= 'Z' ; b++)
+       {
+               putchar (b);
+       }
+	putchar('\n');
+
+		return (0);
+}
